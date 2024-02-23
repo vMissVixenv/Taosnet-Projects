c511d74d1de8a9d5218ffaf506a161f3d7fdc32a
vMissVixenv <160562916+vMissVixenv@users.noreply.github.com>
2024-02-22 21:45:57 -0700

    Update pingfrontend.js

--- pingfrontend.js
+++ pingfrontend.js
@@ -6,27 +6,28 @@ function createPingResultElement(ip, status, responseTime) {
     const statusElement = document.createElement('p');
     const responseTimeElement = document.createElement('p');
     // Set content
-    ipElement.textContent = IP: ${ip};
-    statusElement.textContent = Status: ${status};
-    responseTimeElement.textContent = Response Time: ${responseTime}ms;
+    ipElement.textContent = `IP: ${ip}`;
+    statusElement.textContent = `Status: ${status}`;
+    responseTimeElement.textContent = `Response Time: ${responseTime}ms`;
     // Append elements to container
     container.appendChild(ipElement);
     container.appendChild(statusElement);
     container.appendChild(responseTimeElement);
     return container;
 }
+
 function pingDevice(event) {
     let dialog = document.getElementById('errorMessage');
-dialog.textContent = ''; /* remove all existing child elements from dialog */
+    dialog.textContent = ''; /* remove all existing child elements from dialog */
     event.preventDefault();
     let routerIP = event.target.getAttribute('data-router');
     let smIP = event.target.getAttribute('data-sm');
     // Make API call to ping.php for routerIP
-    fetch('/api/v2/ping/' + routerIP)
+    fetch('/api/v2/ping.php?antip=' + routerIP)
         .then(response => response.json())
         .then(data => {
             // Create and display ping result element
-            const pingResultElement = createPingResultElement(data.ip, data.status, data.response_time);
+            const pingResultElement = createPingResultElement(data.antenna.ip, data.antenna.status, data.antenna.response_time);
             dialog.appendChild(pingResultElement);
         })
         .catch(error => {
@@ -35,12 +36,12 @@ dialog.textContent = ''; /* remove all existing child elements from dialog */
         });
     // separate call for smIP
     if (smIP) {
-        fetch('/api/v2/ping/' + smIP)
+        fetch('/api/v2/ping.php?wanip=' + smIP)
             .then(response => response.json())
             .then(data => {
                 // Create and display ping result element
-                const pingResultElement = createPingResultElement(data.ip, data.status, data.response_time);
-               dialog.appendChild(pingResultElement);
+                const pingResultElement = createPingResultElement(data.router.ip, data.router.status, data.router.response_time);
+                dialog.appendChild(pingResultElement);
             })
             .catch(error => {
                 console.error('Error:', error);
