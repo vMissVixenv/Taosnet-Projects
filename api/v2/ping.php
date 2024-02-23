c7530b27e3cbb5a8d5fb45657497e5a2cbd90253
vMissVixenv <160562916+vMissVixenv@users.noreply.github.com>
2024-02-22 21:47:03 -0700

    Update ping.php

--- api/v2/ping.php
+++ api/v2/ping.php
@@ -44,3 +44,33 @@ if (isset($_GET['router']) && isset($_GET['sm'])) {
     echo json_encode(array('error' => 'IP addresses not provided'));
 }
 
+
+Second option 
+ <?php
+
+// Set headers to return JSON
+header('Content-Type: application/json');
+
+// Function to execute ping command and return result
+function getPingResult($ip) {
+    $pingResult = shell_exec("ping -c 5 -W 1 $ip");
+    // Replace newlines and format ping results
+    return preg_replace(array('/\n/m', '/(\d+% packet loss, time \d+\w+)/'), array('<br />', '<strong>$1</strong>'), $pingResult);
+}
+
+// Check if IP parameters are provided
+if (isset($_GET['antip']) && isset($_GET['wanip'])) {
+    $antPing = getPingResult($_GET['antip']);
+    $wanPing = getPingResult($_GET['wanip']);
+
+    // Return JSON response
+    echo json_encode(array(
+        'antenna' => $antPing,
+        'router' => $wanPing
+    ));
+} else {
+    // Return error if parameters are missing
+    http_response_code(400);
+    echo json_encode(array('error' => 'Missing IP parameters'));
+}
+?>
